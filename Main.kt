1e0a56954b55b9d82a5dc23b5b6b3d6d39a4d80b
lukasreei <130949016+lukasreei@users.noreply.github.com>
2024-06-28 23:48:27 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var nome: String = "Lukas Romero de A. dos Santos"
+    var texto: String
+
+    var numero: Byte = 44
+
+    var pib: Long = 1869000000000
+
+    var PP: Long = 211000000
+    println("o PIB do Brasil é $pib")
+    println("pb do BR ${pib/PP}")
+}
---
+++ comentarios.kt
@@ -0,0 +1,31 @@
+// usado para comentar
+// usasse var para declarar variavel
+// usasse "" para declarar uma string
+
+/**
+ *  type Bit
+ *  Double 64   ponto flutuante   . + numero, força a conversão
+ *  float 32  ponto flutuante    f no final força a conversão
+ *  long 64   numeros inteiros   L no final força a conversão
+ *  int 32  numeros inteiros
+ *  short 16 numeros inteiros max 32767 min -32768
+ *  byte 8  numeros inteiros   max 127 min -128
+ *  boolean ?
+ *  String ?
+ *  char ?/
+ */
+
+//unsigned
+// varia numeros do 0 a 255, retirando a parte negativa
+
+
+//val = valor imutavel
+//var valor mutavel
+
+/**
+ * adição +  ex: var idade = 40       idade += 1
+ * subtraçãoo -
+ * multiplicação *
+ * divisão /   ex: var idade: float = 10 / 3 or var idade: int = 30 / 3
+ * resto da divisão = %   ex var idade = 12 % 5
+ */
