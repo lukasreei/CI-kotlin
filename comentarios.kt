ac1be75573c762d62c6b7cc4e4881205ccb11563
lukasreei <130949016+lukasreei@users.noreply.github.com>
2024-06-29 21:09:51 -0300

    Add files via upload

--- comentarios.kt
+++ comentarios.kt
@@ -29,3 +29,88 @@
  * divisão /   ex: var idade: float = 10 / 3 or var idade: int = 30 / 3
  * resto da divisão = %   ex var idade = 12 % 5
  */
+
+
+// fun  declara função       em seguida o nome da funcção, exemplo: fun helloworld
+
+// fun helloworld(nome: String): String{
+// }
+
+
+// tipo de retorno
+// unit    o tipo unit diz que a funcao não possui retorno
+// double, float, long, int, byte, booelan, string e char
+//instancias de classes    uma função é capaz ded retorna um tipo de dado resultado da instancia de uma classe
+
+//chamada de função
+//        fun helloworld(nome: String): String {
+//           return "Hello $nome"
+//      }
+
+
+
+/**fun main() {
+
+var nome: String = "Lukas Romero de A. dos Santos"
+var texto: String
+
+var numero: Byte = 44
+
+var pib: Long = 1869000000000
+
+var PP: Long = 211000000
+println("o PIB do Brasil é $pib")
+println("pb do BR ${pib/PP}")
+}
+ */
+
+/**fun main(){
+helloworld()
+println("o valor da soma é ${sum(a = 5, b = 10)}")
+}
+fun sum(a: Int, b: Int): Int {
+return (a + b)
+}
+
+fun helloworld(){
+println("Hello World")
+}
+ */
+
+//FUNÇÕES EM UMA UNICA LINHA
+//fun main(){
+//helloWorld()
+//println("a soma é ${sum(a = 10, b = 5)}")
+//   println("divisão ${divisao(a = 9f, b = 3f)}")
+//    println(divisao(a= 12f, b=3.5f))
+//}
+//fun helloWorld(){
+//  println("hello wordl")
+//}
+//fun sum (a: Int, b: Int): Int (a + b)
+//
+//fun divisao (a: Float, b: Float) = (a / b)
+
+//função string
+//fun main(){
+//   val str = "pg kotlin."
+//  println("o tamanho da string: ${str.length}")
+//   println("a posição (index) 0 string: ${str[0]}")
+//   println(str.startsWith("p"))
+// println(str.endsWith("c"))
+//
+//  println(str.substring(1,4))
+// println(str.replace("kotlin", "kotlin é show"))
+//   println(str.lowercase())
+// println(str.uppercase())
+
+//}
+import kotlin.math.*
+//fun main(){
+//  println(max( 5,10))
+//  println(min( 5,10))
+//   println(sqrt(5.67f))
+// println(PI)
+//  println(E)
+// println(round(x = 3142342.2524))
+//}
