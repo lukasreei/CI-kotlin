54b99f44277f5ac06d5d5c52826729eff1f5c03f
lukasreei <130949016+lukasreei@users.noreply.github.com>
2024-06-29 21:09:21 -0300

    Add files via upload

--- Main.kt
+++ Main.kt
@@ -1,13 +1,27 @@
-fun main() {
+import kotlin.math.*
 
-    var nome: String = "Lukas Romero de A. dos Santos"
-    var texto: String
-
-    var numero: Byte = 44
-
-    var pib: Long = 1869000000000
-
-    var PP: Long = 211000000
-    println("o PIB do Brasil é $pib")
-    println("pb do BR ${pib/PP}")
+//atividade 1
+fun converte(anos: Int){
+    println("$anos anos equivalem a: ")
+    println("${anos * 12} meses")
+    println("${anos * 365} dias")
+    println("${anos * 365 * 24} horas")
+    println("${anos * 365 * 24 * 60} minutos")
+    println("${anos * 365 * 24 * 60 * 60} segundos")
 }
+
+fun milhaskm(milhas: Float): Float = milhas * 1.6f
+
+fun caracteres(str: String) = str.length
+
+fun cubo(n: Int) = n * n * n
+
+fun main(){
+    converte(2)
+    println(caracteres("cubo"))
+    cubo(3)
+    println(cubo(3))
+
+
+}
+
